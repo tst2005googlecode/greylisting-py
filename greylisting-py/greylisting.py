175bea778bb8fdde2813c95028ebd0544df7f2f8
jphoude <jphoude@e0561362-7642-0410-9513-a32e42a95c3b>
2007-12-31 02:00:34 +0000

    continued to enhance the script, seems to be working again now

--- greylisting-py/greylisting.py
+++ greylisting-py/greylisting.py
@@ -20,7 +20,7 @@
 # Inspired by the exim.pl script by Roman Festchook ( http://llab.zhitomir.net/?greylist )
 # *****************************************************************************************
 # Parameters : 
-# greylisting.py <local-part> <domain> <sender-address> <sender-host-address>
+# greylisting.py <local-part> <domain> <sender-address> <sender-host-address> <helo-name>
 # 
 # Return values : greylist, ok, spfok, excluded, whitelisted
 # greylist should trigger a 4xx temporary error, you should accept the message for any other
@@ -37,16 +37,19 @@ except ImportError:
     # spf not instelled correctly, don't use it
     USE_SPF = False
 try:
-    # try import ipv4
     from ipv4 import CIDR
     CIDR_LOADED = True
 except ImportError:
     CIDR_LOADED = False
 
+# connect to DB
+db = mysql.connect(DB_HOST, user=DB_USER, passwd=DB_PASSWD, db=DB_NAME)
+cur = db.cursor()
+
 ############################ functions ############################
 
-def testSPF(helo, emailfrom, host):
-    spfResult = spf.check(host, emailfrom, helo)
+def testSPF(helo, mailFrom, hostAddr):
+    spfResult = spf.check(hostAddr, mailFrom, helo)
     
     if spfResult[0] == 'pass':
         # SPF OK, bypass greylist
@@ -59,13 +62,13 @@ def checkEnabled(lp, domain):
        GREYLIST_ENABLE table for each address or domain"""
     cur.execute("SELECT 1 FROM GREYLIST_ENABLE WHERE (username = %s OR username = %s) AND domain = %s", ('%', lp, domain))
     
-    return (localPart not in EXCLUDE_MAILBOXES) and cur.fetchone()
+    return (lp not in EXCLUDE_MAILBOXES) and cur.fetchone()
 
-def checkWhitelisted(host):
-    """check if an host/net is whitelisted"""
+def checkWhitelisted(hostAddr):
+    """check if an hostAddr/net is whitelisted"""
 
-    # check host
-    cur.execute("SELECT 1 FROM HOST_WHITELIST WHERE %s = host", (host, ))
+    # check hostAddr
+    cur.execute("SELECT 1 FROM HOST_WHITELIST WHERE %s = host", (hostAddr, ))
     res = cur.fetchone()
     if res:
         return True
@@ -75,63 +78,73 @@ def checkWhitelisted(host):
         cur.execute("SELECT net FROM NET_WHITELIST")
         net = cur.fetchone()
         while net:
-            if CIDR(host) in CIDR(net[0]):
+            if CIDR(hostAddr) in CIDR(net[0]):
                 # network matches whitelist
                 return True
 
     return False
     
-def returnValue(value):
-    print value
-    sys.exit(0)
-    
-# default result
-result = RESULT_GREYLIST_ERROR
+ 
+
+def greylist(localPart, domain, mailFrom, hostAddr, helo):
+    """runs greylisting process and returns the result"""
+    emailto = "%s@%s" % (localPart, domain)
+
+    # Check if greylisting is enabled for the mailbox or not
+    if not checkEnabled(localPart, domain):
+        return 'excluded'
+        
+    # Check whitelist
+    whiteListRes = checkWhitelisted(hostAddr)
+    if whiteListRes:
+        return 'whitelisted'
+        
+    # Check SPF
+    if USE_SPF and testSPF(helo, mailFrom, hostAddr):
+        return 'spfok'
+
+    # Check current record
+    cur.execute("SELECT UNIX_TIMESTAMP(block_expires)-UNIX_TIMESTAMP() \
+                        FROM GREYLIST WHERE rcpt_to=%s AND \
+                        mail_from=%s AND relay_ip=%s", (emailto, mailFrom, hostAddr))
+    statusArray = cur.fetchone()
+
+    if not statusArray:
+        # Not record found, insert one
+        cur.execute("INSERT INTO GREYLIST VALUES (NULL, %s, %s, %s, \
+                                                  DATE_ADD(NOW(), INTERVAL 15 MINUTE), \
+                                                  DATE_ADD(NOW(), INTERVAL 36 DAY), 1, 0, 0, \
+                                                  'AUTO', NOW(), NULL)", (hostAddr, mailFrom, emailto))
+        return 'greylist'
+        
+    if int(statusArray[0]) > 0:
+        # Ooops, host retryed too soon, it seems. Block message again and log BLOCK.
+        cur.execute("UPDATE GREYLIST SET blocked_count=blocked_count + 1 \
+                            WHERE rcpt_to=%s AND mail_from=%s AND relay_ip=%s", (emailto, mailFrom, hostAddr))
+        
+        return 'greylist'
+    else:
+        # Ok, host is clear to send mail here. log PASS and update expire date (36 days from now)
+        cur.execute("UPDATE GREYLIST SET passed_count=passed_count+1, \
+                                         record_expires=DATE_ADD(NOW(), INTERVAL 36 DAY) \
+                            WHERE rcpt_to=%s AND mail_from=%s AND relay_ip=%s", (emailto, mailFrom, hostAddr))
+        
+        return 'ok'
+
+# main program starts here
 
 # get command line params
-localPart, domain, senderAddr, senderHost, helo = sys.argv[1:5]
+args = sys.argv[1:6]
 
-emailto = "%s@%s" % (localPart, domain)
-emailfrom = senderAddr
-host = senderHost
+if len(args) != 5:
+    print "This commands takes 5 parameters : "
+    print "greylisting.py <local-part> <domain> <sender-address> <sender-host-address> <helo-name>"
+    sys.exit(1)
 
-# connect to DB
-db = mysql.connect(DB_HOST, user=DB_USER, passwd=DB_PASSWD, db=DB_NAME)
-cur = db.cursor()
-
-# Check if greylisting is enabled for the mailbox or not
-if not checkEnabled(localPart, domain):
-    returnValue('excluded')
-    
-# Check whitelist
-whiteListRes = checkWhitelisted(host)
-if whiteListRes:
-    returnValue('whitelisted')
-    
-# Check SPF
-if USE_SPF and testSPF(helo, emailfrom, host):
-    returnValue('spfok')
-
-# Check current record
-cur.execute("SELECT UNIX_TIMESTAMP(block_expires)-UNIX_TIMESTAMP() FROM GREYLIST WHERE rcpt_to=%s AND mail_from=%s AND relay_ip=%s", (emailto, emailfrom, host))
-statusArray = cur.fetchone()
-
-if not statusArray:
-    # Not record found, insert one
-    cur.execute("INSERT INTO GREYLIST VALUES (NULL, %s, %s, %s, DATE_ADD(NOW(), INTERVAL 15 MINUTE), DATE_ADD(NOW(), INTERVAL 36 DAY), 1, 0, 0, 'AUTO', NOW(), NULL)", (host, emailfrom, emailto))
-    returnValue('greylist')
-    
-# Record found, examine results
-if int(statusArray[0]) > 0:
-    # Ooops, host retryed too soon, it seems. Block message again and log BLOCK.
-    cur.execute("UPDATE GREYLIST SET blocked_count=blocked_count+1 WHERE rcpt_to=%s AND mail_from=%s AND relay_ip=%s", (emailto, emailfrom, host))
-    
-    returnValue('greylist')
-else:
-    # Ok, host is clear to send mail here. log PASS and update expire date (36 days from now)
-    cur.execute("UPDATE GREYLIST SET passed_count=passed_count+1,  record_expires=DATE_ADD(NOW(), INTERVAL 36 DAY) WHERE rcpt_to=%s AND mail_from=%s AND relay_ip=%s", (emailto, emailfrom, host))
-    
-    returnValue('ok')
+value = greylist(*args)
 
 # log mail
-cur.execute("INSERT INTO MAIL_LOG VALUES(NULL, NOW(), %s, %s, %s, %s)", (emailto, emailfrom, host, RESULT_TEXT[result]))
+cur.execute("INSERT INTO MAIL_LOG VALUES(NULL, NOW(), %s, %s, %s, %s)", ('%s@%s' % (args[0], args[1]), args[2], args[3], value))
+
+print value
+sys.exit(0)
